30053325e6792bb9bf6d7f91a02d69c6b4215c65
Ajmal Ahamed <ajmal.ahamed@emvigotech.com>
2021-05-30 21:40:17 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # html-to-pdf
-create pdf files from html
+
+Create PDF file from html
+
+## Installation
+
+Use the package manager [npm](https://www.npmjs.com/) to install the dependencies.
+
+```npm install
+
+```
+
+## Target
+
+To create a simple tool to create and preview PDF document from html code
+
+## Dependencies
+
+- [Puppeteer](https://github.com/puppeteer/puppeteer)
+- [Express](https://github.com/expressjs/express)
+
+## Roadmap
+
+- [x] Create PDF file from input html
+- [ ] UI to input html
+- [ ] UI to Preview Html
+- [ ] UI to Preview PDF
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const pdfLib = require("./lib/pdf");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "assets")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "assets/main.html"));
+});
+
+app.post("/generate-pdf", async (req, res) => {
+  const inputHtml = req.body?.html;
+  console.log("req", req.body);
+  if (!inputHtml) {
+    res.json({ message: "html content is required" }).status(400);
+    return;
+  }
+  await pdfLib.generatePdf(inputHtml);
+  res.send("PDF generated");
+});
+
+app.listen(port, () => {
+  console.log(`PDF app listening at http://localhost:${port}`);
+});
